94d6b78566dbad2bfd97774d14ca7b99c2e32710
BIrehan Anteneh <91595957+birehan@users.noreply.github.com>
2022-12-09 21:41:07 +0300

    Add files via upload

--- lib/screens/CarScreen.dart
+++ lib/screens/CarScreen.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class CarScreen extends StatefulWidget {
+  const CarScreen({super.key});
+
+  @override
+  State<CarScreen> createState() => _CarScreenState();
+}
+
+class _CarScreenState extends State<CarScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
