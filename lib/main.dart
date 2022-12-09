d601a15cb61e6a3291b35c9c801f32cbf6bb64bb
GemechisUrgessa <gemechisurgessa@gmail.com>
2022-12-09 22:26:03 +0300

    signUp page

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui/signUp.dart';
 import 'package:flutter_ui/views/home.dart';
 
 import 'login.dart';
 
+// void main() {
+//   runApp(MaterialApp(
+//     // theme: ThemeData(
+//     //     primarySwatch: Colors.green,
+//     //     appBarTheme: AppBarTheme(
+//     //       backgroundColor: Color.fromARGB(255, 6, 22, 241),
+//     //     )),
+//     home: SignUpPageScreen(),
+//     debugShowCheckedModeBanner: false,
+//   ));
+// }
+
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(
-        primarySwatch: Colors.green,
-        appBarTheme: AppBarTheme(
-          backgroundColor: Color.fromARGB(255, 15, 192, 44),
-        )),
-    home: new Home(),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(const MyApp());
 }
 
-// void main(){
-//   runApp(const MyApp());
-// }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: '>>> APP',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: const LoginPageScreen(),
-//     );
-//   }
-// }
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '>>> APP',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SignUpPageScreen(),
+    );
+  }
+}
